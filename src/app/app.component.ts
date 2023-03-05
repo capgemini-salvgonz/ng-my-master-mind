a57f1eb094d4c138d94c42037fe852115ed5ffe0
El Chava <chava.gnolasco@gmail.com>
2023-03-05 11:41:37 -0600

    assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GameStorageService } from './service/game-storage.service';
+import { GameStatistics } from './model/game.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My master mind';
+  storage: GameStorageService;
+  statistics: GameStatistics | null;
+  name: string = '';
+
+  constructor(storage: GameStorageService) {
+    this.storage = storage;
+    this.statistics = this.storage.statistics;
+  }
+
+  saveStatistics(statistics: GameStatistics) {
+    this.statistics = statistics;
+    this.storage.saveStatistics(statistics);
+  }
+
+  onClickEnter() {
+    this.saveStatistics({
+      name: this.name.toUpperCase(),
+      won: 0,
+      lost: 0,
+    });
+    this.name = '';
+  }
 }
