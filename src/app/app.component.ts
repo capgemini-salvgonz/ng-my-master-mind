2852d53219129245e195a1730c7ccead9b44f8bb
El Chava <chava.gnolasco@gmail.com>
2023-03-09 18:51:38 -0600

    option selected and board layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Directive, Input, Output } from '@angular/core';
 import { GameStorageService } from './service/game-storage.service';
 import { GameStatistics } from './model/game.model';
+
+type OptionSelected =
+  'red' | 'blue' | 'yellow' | 'green' |
+  'orange' | 'gray' | 'alpha';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'My master mind';
   storage: GameStorageService;
   statistics: GameStatistics | null;
   name: string = '';
+  @Output() optionSelected: OptionSelected;
 
   constructor(storage: GameStorageService) {
     this.storage = storage;
     this.statistics = this.storage.statistics;
+    this.optionSelected = 'alpha';
   }
 
   saveStatistics(statistics: GameStatistics) {
@@ -30,5 +37,9 @@ export class AppComponent {
       lost: 0,
     });
     this.name = '';
+  }
+
+  onclickOption(option: OptionSelected): void {
+    this.optionSelected = option;
   }
 }
